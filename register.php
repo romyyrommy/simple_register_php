08c45803e9e8ac30e18fe0c1dc1adac5a8d8db2c
romyyrommy <rovshans1994@gmail.com>
2019-04-24 18:07:19 +0400

    Include header

--- register.php
+++ register.php
@@ -7,6 +7,16 @@
 	<link rel="stylesheet" href="css/fonts.css">
 </head>
 <body>
-	
+	<header>		
+		<ul>
+			<div class="log">
+				<li><a href="index.php">Logo</a></li>
+			</div>
+			<div class="singbut">
+				<li><a href="login.php">Log in</a></li>
+				<li><a href="register.php">Sing up</a></li>
+			</div>
+		</ul>	
+	</header>
 </body>
 </html>
