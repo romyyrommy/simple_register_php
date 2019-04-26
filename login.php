d52b8788f2e79e2c133aed1cbdf28ebeda6b3395
romyyrommy <rovshans1994@gmail.com>
2019-04-26 10:47:42 +0400

    Ceate login file

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+</head>
+<body>
+	<header>		
+		<ul>
+			<div class="log">
+				<li><a href="index.php">Logo</a></li>
+			</div>
+			<div class="singbut">
+				<li><a href="login.php">Log in</a></li>
+				<li><a href="register.php">Sing up</a></li>
+			</div>
+		</ul>	
+	</header>
+	<section>
+		<div class="centersing">
+			<form action="" method="POST">
+				<input type="text" name="username" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
+				<div class="doregit">
+					<input type="button" name="do_register" value="Register">
+				</div>
+			</form>
+		</div>
+	</section>
+</body>
+</html>
